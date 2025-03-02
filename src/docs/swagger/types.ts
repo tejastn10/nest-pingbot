3d15f198e7fca16f921ffda321201b7ce0c1fbf1
Tejas Nikhar <tejastn10@gmail.com>
2025-03-02 10:43:42 +0530

    refactor: remove Swagger module and related files

--- src/docs/swagger/types.ts
+++
@@ -1,7 +0,0 @@
-import { InfoObject, TagObject } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
-
-type SwaggerDocumentBuilderOptions = InfoObject;
-
-type Tags = TagObject;
-
-export { SwaggerDocumentBuilderOptions, Tags };
